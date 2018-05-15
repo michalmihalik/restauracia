d8a233e3718691f67fa5a6d63ff5260fa3b8206a
Michal Mihalik <mimi555000@azet.sk>
2018-05-15 21:59:39 +0200

    grafy

--- application/views/restauracia.php
+++ application/views/restauracia.php
@@ -8,6 +8,118 @@ foreach($css_files as $file): ?>
 	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
 <?php endforeach; ?>
 
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Udalost', 'Pocet'],
+          ['Oslava narodenín',     2],
+          ['Kar',      1],
+          ['Ples',  1],
+          ['Stužková', 1]
+        ]);
+
+        var options = {
+          title: 'Najčastejší typ udalostí',
+          backgroundColor: { fill:'transparent' },
+          legendTextStyle: { color: '#FFF' },
+    	  titleTextStyle: { color: '#FFF' }
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Udalost', 'Pocet'],
+          ['1-20 ľudí',     2],
+          ['21-50 ľudí',      1],
+          ['51-80 ľudí',      1],
+          ['81+ ľudí',      1],
+        ]);
+
+        var options = {
+          title: 'Veľkosť udalostí',
+          backgroundColor: { fill:'transparent' },
+          legendTextStyle: { color: '#FFF' },
+    	  titleTextStyle: { color: '#FFF' }
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart2'));
+
+        chart.draw(data, options);
+      }
+    </script>
+
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Vyzdoba', 'Cena'],
+          ['50€-100€',     1],
+          ['100€-200€',      3],
+          ['200€+',  1]
+        ]);
+
+        var options = {
+          title: 'Ceny výzdob',
+          backgroundColor: { fill:'transparent' },
+          legendTextStyle: { color: '#FFF' },
+    	  titleTextStyle: { color: '#FFF' }
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart3'));
+
+        chart.draw(data, options);
+      }
+    </script>
+
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Účinkujúci', 'Pocet účinkovaní'],
+          ['Cigánski diabli',     1],
+          ['Rytmus',      1],
+          ['Tanečníci Viva',  1],
+          ['OldSchoolBrothers', 1],
+          ['Samo Tomeček', 1]
+        ]);
+
+        var options = {
+          title: 'Účinkujúci na udalostiach',
+          backgroundColor: { fill:'transparent' },
+          legendTextStyle: { color: '#FFF' },
+    	  titleTextStyle: { color: '#FFF' }
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart4'));
+
+        chart.draw(data, options);
+      }
+    </script>
+
+
+
 <style> /* Add a black background color to the top navigation */
 .topnav {
     background-color: #333;
@@ -62,5 +174,19 @@ foreach($css_files as $file): ?>
     <?php foreach($js_files as $file): ?>
         <script src="<?php echo $file; ?>"></script>
     <?php endforeach; ?>
+
+<table style="width:100%">
+  <tr>
+    <td><div id="piechart" style="width: 900px; height: 500px;"></div></td>
+    <td><div id="piechart2" style="width: 900px; height: 500px;"></div></td> 
+  </tr>
+  <tr>
+    <td><div id="piechart3" style="width: 900px; height: 500px;"></div></td>
+    <td><div id="piechart4" style="width: 900px; height: 500px;"></div></td> 
+  </tr>
+</table>
+
+    
+    
 </body>
 </html>
